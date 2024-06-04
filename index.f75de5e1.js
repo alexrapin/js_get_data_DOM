bd92bd04d026c050d6e8dd98c658708ff4174898
Olexandr Rapin <alexrapin04@gmail.com>
2024-06-04 14:15:20 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+// write your code here
+const populations = [
+    ...document.querySelectorAll(".population")
+].map((popul)=>+popul.textContent.split(",").join(""));
+const total = populations.reduce((acum, val)=>acum + val, 0);
+const average = total / populations.length;
+document.querySelector(".total-population").textContent = total.toLocaleString("en-US");
+document.querySelector(".average-population").textContent = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
